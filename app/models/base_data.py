c35f44945b59eb5604b54fb370288ceea2ad1df3
ypgsh <huazhong_2006@163.com>
2020-07-23 16:41:58 +0800

    fix(model): update model define

--- app/models/base_data.py
+++ app/models/base_data.py
@@ -17,45 +17,48 @@ from app.models.base import Base
 class ValveProducts(Base):
     __tablename__ = 'valve_product_table'
     model_id = db.column(db.string(128))      #
-    model_version = db.column(db.string(128))
-    series_id = db.column(db.string(128))
+    model_version = db.column(db.string(32))
+    series_id = db.column(db.integer)
     cad_status = db.column(db.string(128))
-    press_class = db.column(db.string(128))
-    temp_class = db.column(db.string(128))
-    press_design = db.column(db.string(128))
-    temp_design = db.column(db.string(128))
-    stop_throat_diam = db.column(db.string(128))
-    gov_throat_diam = db.column(db.string(128))
-    ovld_throat_diam = db.column(db.string(128))
-    stop_stem_lift = db.column(db.string(128))
-    gov_stem_lift = db.column(db.string(128))
-    ovld_stem_lift = db.column(db.string(128))
-    shape_id = db.column(db.string(128))
-    cv_calc = db.column(db.string(128))
-    cv = db.column(db.string(128))
-    casing_mat = db.column(db.string(128))
-    stem_mat = db.column(db.string(128))
-    dim_a = db.column(db.string(128))
-    dim_b = db.column(db.string(128))
-    dim_c = db.column(db.string(128))
-    dim_d = db.column(db.string(128))
-    dim_e_l = db.column(db.string(128))
-    fluid_mode = db.column(db.string(128))
+    press_class = db.column(db.integer)
+    temp_class = db.column(db.integer)
+    press_design = db.column(db.FLOAT)
+    temp_design = db.column(db.FLOAT)
+    stop_throat_diam = db.column(db.FLOAT)
+    gov_throat_diam = db.column(db.FLOAT)
+    ovld_throat_diam = db.column(db.FLOAT)
+    stop_stem_lift = db.column(db.FLOAT)
+    stop_stem_lift_main = db.column(db.FLOAT)
+    stop_stem_lift_eql = db.column(db.FLOAT)
+    gov_stem_lift = db.column(db.FLOAT)
+    ovld_stem_lift = db.column(db.FLOAT)
+    shape_id = db.column(db.string(32))
+    cv_calc = db.column(db.FLOAT)
+    cv = db.column(db.FLOAT)
+    casing_mat_name = db.column(db.string(128))
+    stem_mat_name = db.column(db.string(128))
+    feature_size_a = db.column(db.FLOAT)
+    feature_size_b = db.column(db.FLOAT)
+    feature_size_c = db.column(db.FLOAT)
+    feature_size_d = db.column(db.FLOAT)
+    feature_size_e_l = db.column(db.FLOAT)
+    fluid_medium_name = db.column(db.string(128))
     gov_mode = db.column(db.string(128))
     install_mode = db.column(db.string(128))
     act_oil_press_mode = db.column(db.string(128))
-    eql_roll = db.column(db.string(128))
+    pilot_speedup = db.column(db.BOOLEAN)
     out_conn_type = db.column(db.string(128))
     act_conn_type = db.column(db.string(128))
-    cost_level = db.column(db.string(128))
-    weight_total = db.column(db.string(128))
-    industry = db.column(db.string(128))
+    cost_level = db.column(db.string(32))
+    total_weight = db.column(db.FLOAT)
+    industry = db.column(db.BOOLEAN)
     dwg_id = db.column(db.string(128))
-    turbine_id = db.column(db.string(128))
+    turbine_unit_ids = db.column(ARRAY(db.string(64)))
     description = db.column(db.string(128))
     comments = db.column(db.string(128))
-    used = db.column(db.string(128))
+    used = db.column(db.BOOLEAN)
     deleted = db.Column(db.Boolean, default=False)
+
     def delete(self):
         self.extra_info.delete()
         db.session.delete(self)
@@ -77,7 +80,7 @@ class Speed(Base):
     unit_run_mode = db.column(db.string(128))
     press_loss_range = db.column(ARRAY(db.float))
     speed_range = db.column(ARRAY(db.float))
-    suitable_unit = db.column(db.string(128))
+    suitable_turbine_unit = db.column(db.string(128))
     comments = db.column(db.string(128))
 
     def delete(self):
@@ -89,7 +92,7 @@ class ValveSeries(Base):
     code = db.column(db.string(128))
     structure = db.column(db.string(128))
     use_scene = db.column(db.string(128))
-    sets = db.column(ARRAY(db.string(32)))
+    typical_turbine_units = db.column(ARRAY(db.string(32)))
     sketch = db.column(db.text)
 
 class Pressure(Base):
@@ -104,14 +107,4 @@ class Temperature(Base):
 
 
 class ValveProductSchema(marshmallow.Schema):
-    # validate
-    project_code = String(validate=validate.Length(max=128), required=True)
-    type = String(validate=validate.Length(max=32), required=True)
-
-    # transform
-    id = Function(lambda obj: obj.tid)
-    creator_name = Function(lambda obj: obj.creator_id)
-    create_time = DateTime()
-
-    class Meta:
-        fields = ('id', 'name', 'project_code', 'type', 'version', 'office_name', 'creator_name', 'create_time')
+    pass
